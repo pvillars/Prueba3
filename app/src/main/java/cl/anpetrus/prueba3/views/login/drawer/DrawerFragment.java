3789ec0d8e5daa4a72fbc382678d7b64704033e6
Petrus <pvillar@hotmail.cl>
2017-08-26 20:12:20 -0300

    Login Basico con botones

---
+++ app/src/main/java/cl/anpetrus/prueba3/views/login/drawer/DrawerFragment.java
@@ -0,0 +1,30 @@
+package cl.anpetrus.prueba3.views.login.drawer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cl.anpetrus.prueba3.R;
+
+public class DrawerFragment extends Fragment {
+
+
+    public DrawerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_drawer_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
